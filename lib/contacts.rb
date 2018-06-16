5037bd9ca93ba8e8a939fa0beef36154d2faed35
Kevin Chi <kevinchi118@gmail.com>
2018-06-16 00:25:00 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
     contacts.each do |person, details|
         details.each do |category, value|
             if category == :favorite_ice_cream_flavors
-                category.each do |flavor|
+                value.each do |flavor|
                     if flavor == "strawberry"
                         category.delete(flavor)
                     end
